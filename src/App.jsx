be51f0e5e614282a9020a46d6a74e117e03cdcc7
Maksym Pecheniuk <pecheniuk.maksym@gmail.com>
2019-03-29 18:10:12 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+// import { hot } from "react-hot-loader";
+
+import  "./App.css";
+
+const App = () => {
+	return <div className="app">react dgdStarter </div>;
+};
+
+export default App;
